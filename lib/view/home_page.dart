687722447ead5d1377a91ebea3b6776ab1bc51cc
alish <alishakoori89@gmail.com>
2022-08-20 19:34:13 +0430

    add navigation button

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:khadamat_behesht_zahra/presentation/google_icons.dart';
+import 'package:khadamat_behesht_zahra/presentation/my_flutter_app_icons.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
+
+  final List<Widget> _children = [
+    ContactPage(),
+    CallLogs(),
+    FavoriteContacts()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +33,42 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.green,
         title: const Text("درخواست آنلاین خدمات بهشت زهرا(س)",
         style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600)),
-        actions: [
-
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(MyFlutterApp.panorama_wide_angle, color: Colors.black54,),
+          ),
         ],
         leading: IconButton(
           onPressed: (){},
-          icon: Icon(Google.basket),
+          icon: const Icon(Google.basket, color: Colors.black54),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contacts),
+              label: 'Contact',
+              backgroundColor: Colors.grey[300]
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.access_time_rounded),
+              label: 'Resent',
+              backgroundColor: Colors.grey[300]
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'Favorites',
+              backgroundColor: Colors.grey[300]
+          ),
+        ],
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.black,
+          iconSize: 25,
+          onTap: _onItemTapped,
+          elevation: 5),
+        body: _children[_selectedIndex]
     );
   }
 }
